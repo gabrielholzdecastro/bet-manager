ae074d4289ce3a8206f27f13af1f77d29d08df2f
gabriel.castro <gabriel.castro@el.com.br>
2023-09-26 15:27:13 -0300

    first commit

---
+++ src/test/java/br/com/gabriel/bet/BetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.bet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
